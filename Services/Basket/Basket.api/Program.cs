ce532d8e79fa722c51c70aa6243268dc0eac17c0
Milad Eghlimapour <m.eghlimapour@gmail.com>
2022-10-16 12:28:02 +0330

    Use Redis Cache for save user basket Add Model Shopping Card for basket Add Repository Basket and Implement Implement basket controller

--- Services/Basket/Basket.api/Program.cs
+++ Services/Basket/Basket.api/Program.cs
@@ -1,11 +1,20 @@
+using Basket.api.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager Configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddStackExchangeRedisCache(option =>
+{
+    option.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 
---
+++ Services/Basket/Basket.api/Repositories/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.api.Entities;
+
+namespace Basket.api.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetUserBasket(string username);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string username);
+    }
+}
